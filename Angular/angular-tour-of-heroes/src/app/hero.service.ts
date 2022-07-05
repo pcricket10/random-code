e369bd672f2366b536c22606229398a58b247854
Philip McCrickard <pcricket10@gmail.com>
2022-07-05 17:43:37 -0400

    started get data from server heroes

--- Angular/angular-tour-of-heroes/src/app/hero.service.ts
+++ Angular/angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, map, tap, Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -9,11 +10,18 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+  private log(message: string) {
+    this.messageService.add(`HeroService" ${message}`);
+  }
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes')
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    )
   }
 
   getHero(id: number): Observable<Hero> {
