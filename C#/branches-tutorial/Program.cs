94b95dba227b241297f3a643738828570eaf419d
Philip McCrickard <pcricket10@gmail.com>
2022-08-17 17:35:42 -0400

    csharp loops

--- C#/branches-tutorial/Program.cs
+++ C#/branches-tutorial/Program.cs
@@ -1,17 +1,42 @@
-﻿int a = 5;
-int b = 3;
-int c = 4;
-if (a + b > 10)
-  Console.WriteLine("The answer is greater than 10");
-else
-  Console.WriteLine("The answer is not greater than 10");
-if ((a + b + c > 10) || (a == b))
+﻿// ExploreIf();
+for (int index = 0; index < 10; index++)
 {
-  Console.WriteLine("The answer is greater than 10");
-  Console.WriteLine("Or the first number is equal to the second");
+  Console.WriteLine($"Hello World! The index is {index}");
 }
-else
+
+void ExploreIf()
 {
-  Console.WriteLine("The answer is not greater than 10");
-  Console.WriteLine("And the first number is not equal to the second");
+  int a = 5;
+  int b = 3;
+  if (a + b > 10)
+  {
+    Console.WriteLine("The answer is greater than 10");
+  }
+  else
+  {
+    Console.WriteLine("The answer is not greater than 10");
+  }
+
+  int c = 4;
+  if ((a + b + c > 10) && (a > b))
+  {
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("And the first number is greater than the second");
+  }
+  else
+  {
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("Or the first number is not greater than the second");
+  }
+
+  if ((a + b + c > 10) || (a > b))
+  {
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("Or the first number is greater than the second");
+  }
+  else
+  {
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("And the first number is not greater than the second");
+  }
 }
