a6f3b6823324103fde22be25d64049ebd242dc73
Philip McCrickard <pcricket10@gmail.com>
2022-09-07 19:12:46 -0400

    MySuperBank

--- C#/MySuperBank/Program.cs
+++ C#/MySuperBank/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using mySuperBank;
+
+var account = new BankAccount("Philip", 10000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+// Console.WriteLine(account.Balance);
+
+account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+// Console.WriteLine(account.Balance);
+
+Console.WriteLine(account.GetAccountHistory());
+
+
+
+
+
+
+// // Test that the initial balances must be positive.
+// try
+// {
+//   var invalidAccount = new BankAccount("invalid", -55);
+// }
+// catch (ArgumentOutOfRangeException e)
+// {
+//   Console.WriteLine("Exception caught creating account with negative balance");
+//   Console.WriteLine(e.ToString());
+// }
+
+// // Test for a negative balance.
+// try
+// {
+//   account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+// }
+// catch (InvalidOperationException e)
+// {
+//   Console.WriteLine("Exception caught trying to overdraw");
+//   Console.WriteLine(e.ToString());
+// }
