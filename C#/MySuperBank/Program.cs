746080e4bb6b46eacd01cef0f5dc47c61e4954ae
Philip McCrickard <pcricket10@gmail.com>
2022-09-09 02:14:18 -0400

    dotnet

--- C#/MySuperBank/Program.cs
+++ C#/MySuperBank/Program.cs
@@ -4,11 +4,13 @@ using mySuperBank;
 var account = new BankAccount("Philip", 10000);
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
 
-account.MakeWithdrawal(120, DateTime.Now, "Hammock");
-// Console.WriteLine(account.Balance);
-
 account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
-// Console.WriteLine(account.Balance);
+account.MakeWithdrawal(5, DateTime.Now, "Coffee");
+account.MakeWithdrawal(5, DateTime.Now, "Diet Coke");
+account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+account.MakeWithdrawal(7, DateTime.Now, "Tea");
+account.MakeWithdrawal(8, DateTime.Now, "Pants");
+
 
 Console.WriteLine(account.GetAccountHistory());
 
---
+++ dotnet/nuget/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Account account = new Account
+{
+  Name = "John Doe",
+  Email = "john@nuget.org",
+  DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+};
+
+string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+Console.WriteLine(json);
+
+public class Account
+{
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public DateTime DOB { get; set; }
+}
