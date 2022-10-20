c9ed072e2b5e66086050287b077a6c63c42ad518
Philip McCrickard <pcricket10@gmail.com>
2022-10-20 14:50:33 -0400

    ruby stuff

---
+++ Ruby/comments.rb
@@ -0,0 +1,19 @@
+# This is a comment line - it explains that the next line of code displays a welcome message
+print "Welcome to Ruby!"
+
+# This is a comment line
+# it explains that the next line of code displays 
+# a welcome message
+
+print "Welcome to Ruby!"  # prints the welcome message
+
+=begin
+This is a comment line
+it explains that the next line of code displays
+a welcome message
+=end
+
+=begin
+print "Welcome to Ruby Essentials!"
+print "Everything you need to know about Ruby"
+=end
---
+++ Ruby/variables.rb
@@ -0,0 +1,51 @@
+MYCONSTANT = "hello"
+
+# MYCONSTANT = "hello2"
+y=10;
+
+=begin
+a = 10
+b = 20
+c = 30
+d = 40
+
+=end
+
+# a,b,c,d = 10,20,30,40
+#
+# puts y.kind_of? Integer
+
+#
+# puts y.class
+
+#
+# s = "hello"
+#
+# puts s.class
+
+#
+# x = 10
+#
+# puts x.class
+
+#
+# x = "hello"
+#
+# puts x.class
+
+# y = 20
+# puts y
+# puts y.class
+#
+# y.to_f
+# puts y
+# puts y.class
+#
+# s = 54321.to_s
+# puts s
+# puts s.class
+
+puts 54321.to_s(2)
+puts 54321.to_s(16)
+puts 54321.to_s(8)
+
